170d444ad59d75977cc38fa00f5adfa5853e6f03
KarmaFail <50875605+KarmaFail@users.noreply.github.com>
2020-06-29 14:04:33 +0200

    Create TestLVM.go

---
+++ terraform/TestLVM.go
@@ -0,0 +1,24 @@
+provider "proxmox" {
+    pm_tls_insecure = true
+    pm_api_url = "https://195.154.154.121:8006/api2/json"
+    pm_user = "root@pam"
+    pm_password = "Espoir15"
+}
+
+resource "proxmox_lxc" "lxc-test" { 
+    hostname = "lxc-test-host"
+    cores = 1
+    memory = "1024"
+    swap = "2048"
+    network {
+        name = "eth0"
+        bridge = "vmbr2"
+        ip = "192.168.20.15/24"  
+    }
+    ostemplate = "local:debian-10.0-standard_10.0-1_amd64.tar.gz" 
+    password = "Espoir15"
+    rootfs = "local:201" 
+    storage = "local"
+    target_node = "proxmox"
+    unprivileged = true
+}
