46cf27dca289b7fbd182c52ab9293e5bef737333
KarmaFail <50875605+KarmaFail@users.noreply.github.com>
2020-06-30 10:22:20 +0200

    Update TestLVM.tf

--- terraform/TestLVM.tf
+++ terraform/TestLVM.tf
@@ -1,4 +1,5 @@
 provider "proxmox" {
+    pm_tls_insecure = true
     pm_api_url = "https://195.154.154.121:8006/api2/json"
     pm_user = "root@pam"
     pm_password = "Espoir15"
@@ -18,6 +19,6 @@ resource "proxmox_lxc" "lxc-test" {
     password = "Espoir15"
     rootfs = "local:201" 
     storage = "local"
-    target_node = "proxmox"
+ #   target_node = "proxmox"
     unprivileged = true
 }
