f42404c0bb2f30cd78ced9d3e3de01bd1b514a30
KarmaFail <50875605+KarmaFail@users.noreply.github.com>
2020-07-11 17:37:09 +0200

    njkl

--- terraform/nextcloud/Redis/Redis.tf
+++ terraform/nextcloud/Redis/Redis.tf
@@ -9,7 +9,7 @@ resource "proxmox_lxc" "RedisN-1" {
         ip = "192.168.20.31/24" 
         gw = "192.168.20.254" 
     }
-    ostemplate = "local:vztmpl/template-debian_v1-0.tar.gz" 
+    ostemplate = "local:vztmpl/template-debian9_v1-0.tar.gz" 
     password = "Espoir15"
     vmid = "231"
     rootfs = "local:231" 
@@ -29,7 +29,7 @@ resource "proxmox_lxc" "RedisN-2" {
         ip = "192.168.20.32/24"  
         gw = "192.168.20.254"
     }
-    ostemplate = "local:vztmpl/template-debian_v1-0.tar.gz" 
+    ostemplate = "local:vztmpl/template-debian9_v1-0.tar.gz" 
     password = "Espoir15"
     vmid = "232"
     rootfs = "local:232" 
