0c8fc9c4c40bc7e3671587907a900f4e8b11dfe4
KarmaFail <50875605+KarmaFail@users.noreply.github.com>
2020-07-13 23:19:38 +0200

    zegtf

--- ansible/nextcloud/config.php
+++ ansible/nextcloud/config.php
@@ -1,32 +1,36 @@
 <?php
 $CONFIG = array (
-    'trusted_domains' =>
+  'objectstore' =>
+  array (
+    'class' => '\\OC\\Files\\ObjectStore\\S3',
+    'arguments' =>
     array (
-      0 => '192.168.20.80',
-      1 => '192.168.20.21',
-      2 => '192.168.20.22',
-      3 => 'nextcloud.maildudek.fr'
+      'bucket' => 'nextcloud',
+      'autocreate' => true,
+      'key' => 'adminesgi',
+      'secret' => 'adminesgi',
+      'hostname' => '192.168.30.80',
+      'port' => 9000,
+      'use_ssl' => false,
+      'use_path_style' => true,
     ),
-    'objectstore' => array(
-        'class' => '\\OC\\Files\\ObjectStore\\S3',
-        'arguments' => array(
-            'bucket' => 'nextcloud',
-            'autocreate' => true,
-            'key'    => 'adminesgi',
-            'secret' => 'adminesgi',
-            'hostname' => 'http://192.168.30.90:9000/',
-            'port' => 80,
-            'use_ssl' => false,
-            'use_path_style'=>true
-        ),
-    ),
-    'memcache.local' => '\\OC\\Memcache\\APCu',
-    'memcache.distributed' => '\\OC\\Memcache\\Redis',
-    'memcache.locking' => '\\OC\\Memcache\\Redis',
-    'filelocking.enabled' => true,
-    'redis' =>
-        array (
-            'host' => '192.168.20.80',
-            'port' => 6379
-            ),
+  ),
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'memcache.distributed' => '\\OC\\Memcache\\Redis',
+  'memcache.locking' => '\\OC\\Memcache\\Redis',
+  'filelocking.enabled' => true,
+  'redis' =>
+  array (
+    'host' => '192.168.20.90',
+    'port' => 6379,
+    'password' => 'Espoir15',
+  ),
+  'dbtype' => 'pgsql',
+  'version' => '19.0.0.12',7
+  'dbname' => 'nextcloud',
+  'dbhost' => '192.168.20.90:5432',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'nextcloud',
+  'dbpassword' => 'nextcloud',
 );
